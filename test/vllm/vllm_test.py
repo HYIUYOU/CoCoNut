b48c7905414449b31c6ce3cf2d68ef90a9d58e28
HYIUYOU <821219285@qq.com>
2024-11-24 12:12:40 +0000

    vllm

--- test/vllm/vllm_test.py
+++ test/vllm/vllm_test.py
@@ -1,80 +1,28 @@
 from vllm import LLM, SamplingParams
-
-llm = LLM(model="/root/heyiyuan/Models/Llama-2-7b-chat-hf")
-sampling_params = SamplingParams(temperature=0.5)
-
-
-def print_outputs(outputs):
-    for output in outputs:
-        prompt = output.prompt
-        generated_text = output.outputs[0].text
-        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
-    print("-" * 80)
-
-
-print("=" * 80)
-
-# In this script, we demonstrate how to pass input to the chat method:
-
-conversation = [
-    {
-        "role": "system",
-        "content": "You are a helpful assistant"
-    },
-    {
-        "role": "user",
-        "content": "Hello"
-    },
-    {
-        "role": "assistant",
-        "content": "Hello! How can I assist you today?"
-    },
-    {
-        "role": "user",
-        "content": "Write an essay about the importance of higher education.",
-    },
+from vllm.model_executor.models.llama import layer_time
+prompts = [
+    "What is the role of gravity in the solar system?",
 ]
-outputs = llm.chat(conversation,
-                   sampling_params=sampling_params,
-                   use_tqdm=False)
-print_outputs(outputs)
+sampling_params = SamplingParams(temperature=0.8, top_p=0.1,max_tokens=1)
 
-# You can run batch inference with llm.chat API
-conversation = [
-    {
-        "role": "system",
-        "content": "You are a helpful assistant"
-    },
-    {
-        "role": "user",
-        "content": "Hello"
-    },
-    {
-        "role": "assistant",
-        "content": "Hello! How can I assist you today?"
-    },
-    {
-        "role": "user",
-        "content": "Write an essay about the importance of higher education.",
-    },
-]
-conversations = [conversation for _ in range(10)]
 
-# We turn on tqdm progress bar to verify it's indeed running batch inference
-outputs = llm.chat(messages=conversations,
-                   sampling_params=sampling_params,
-                   use_tqdm=True)
-print_outputs(outputs)
+llm = LLM(model="/root/heyiyuan/CoCoNut/Models/Llama-2-7b-chat-hf")
 
-# A chat template can be optionally supplied.
-# If not, the model will use its default chat template.
+outputs = llm.generate(prompts, sampling_params)
 
-# with open('template_falcon_180b.jinja', "r") as f:
-#     chat_template = f.read()
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
-# outputs = llm.chat(
-#     conversations,
-#     sampling_params=sampling_params,
-#     use_tqdm=False,
-#     chat_template=chat_template,
-# )
+print("=="*50)
+
+print(len(layer_time))
+print(len(layer_time[0]))
+print(len(layer_time[1]))
+print(len(layer_time[10]))
+print(len(layer_time[21]))
+print(len(layer_time[30]))
+# print(layer_time[10])
+# print(layer_time[11])
+print("=="*50)
